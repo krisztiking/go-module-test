7cb414580836924464149cded63eb9b31aa13832
Krisztian Domokos <domokoskrisztian7@gmail.com>
2024-10-12 16:30:51 +0200

    update struct visibilty 2

---
+++ handler.go
@@ -0,0 +1,99 @@
+package myResp
+
+import (
+	"fmt"
+	"sync"
+)
+
+var Handlers = map[string]func([]Value) Value{
+	"PING": ping,
+	"SET":  set,
+	"GET":  get,
+	"HSET": hset,
+	"HGET": hget,
+}
+
+func ping(args []Value) Value {
+	if len(args) == 0 {
+		return Value{Typ: "string", Str: "PONG"}
+	}
+
+	return Value{Typ: "string", Str: args[0].Bulk}
+}
+
+var SETs = map[string]string{}
+var SETsMu = sync.RWMutex{}
+
+func set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{Typ: "error", Str: "ERR wrong number of arguments for 'set' command"}
+	}
+
+	key := args[0].Bulk
+	value := args[1].Bulk
+
+	SETsMu.Lock()
+	SETs[key] = value
+	SETsMu.Unlock()
+	fmt.Println("Sets updated")
+	return Value{Typ: "string", Str: "OK"}
+}
+
+func get(args []Value) Value {
+	if len(args) != 1 {
+		return Value{Typ: "error", Str: "ERR wrong number of arguments for 'get' command"}
+	}
+
+	key := args[0].Bulk
+
+	SETsMu.RLock()
+	value, ok := SETs[key]
+	SETsMu.RUnlock()
+
+	if !ok {
+		return Value{Typ: "null"}
+	}
+	fmt.Println("Get - Done")
+	return Value{Typ: "bulk", Bulk: value}
+}
+
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutex{}
+
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{Typ: "error", Str: "ERR wrong number of arguments for 'hset' command"}
+	}
+
+	hash := args[0].Bulk
+	key := args[1].Bulk
+	value := args[2].Bulk
+
+	HSETsMu.Lock()
+	if _, ok := HSETs[hash]; !ok {
+		HSETs[hash] = map[string]string{}
+	}
+	HSETs[hash][key] = value
+	HSETsMu.Unlock()
+
+	return Value{Typ: "string", Str: "OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{Typ: "error", Str: "ERR wrong number of arguments for 'hget' command"}
+	}
+
+	hash := args[0].Bulk
+	key := args[1].Bulk
+
+	HSETsMu.RLock()
+	value, ok := HSETs[hash][key]
+	HSETsMu.RUnlock()
+
+	if !ok {
+		return Value{Typ: "null"}
+	}
+
+	return Value{Typ: "bulk", Bulk: value}
+}
--- resp.go
+++ resp.go
@@ -1,4 +1,4 @@
-package myresp
+package myResp
 
 import (
 	"bufio"
